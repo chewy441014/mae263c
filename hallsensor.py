b8001ddb3e3570ba2f69cfb382928f831048f1c6
chewy441014 <p.hill.ultimate@gmail.com>
2018-05-20 01:01:26 -0700

    added code for two encoders, channel A and B

--- hallsensor.py
+++ hallsensor.py
@@ -9,11 +9,15 @@ def sensorCallback(channel):
 	stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
 	if io.input(channel):
 		print("Sensor HIGH" + stamp)
+		sig = 1
 	else:
 		print("Sensor LOW" + stamp)
-		
+		sig = 0
+	return sig
+	
 def main():
-	sensorCallback(22)
+	A = sensorCallback(22)
+	B = sensorCallback(15)
 	try:
 		while True:
 			time.sleep(0.1)
@@ -24,6 +28,8 @@ io.setmode(io.BOARD)
 print("Setup GPIO pin as input on GPIO17")
 io.setup(22, io.IN, pull_up_down=io.PUD_UP)
 io.add_event_detect(22, io.BOTH, callback=sensorCallback, bouncetime=200)
+io.setup(15, io.IN, pull_up_down=io.PUD_UP)
+io.add_event_detect(15, io.BOTH, callback=sensorCallback, bouncetime=200)
 
 if __name__=="__main__":
 	main()
