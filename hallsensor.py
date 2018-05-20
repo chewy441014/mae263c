9ab5d8dafd70f87bb7194fdf2dc93f96fd887336
chewy441014 <p.hill.ultimate@gmail.com>
2018-05-20 00:47:18 -0700

    changed channel

--- hallsensor.py
+++ hallsensor.py
@@ -13,17 +13,17 @@ def sensorCallback(channel):
 		print("Sensor LOW" + stamp)
 		
 def main():
-	sensorCallback(17)
+	sensorCallback(22)
 	try:
 		while True:
 			time.sleep(0.1)
 	except KeyboardInterrupt:
 		io.cleanup()
 
-io.setmode(io.BCM)
+io.setmode(io.BOARD)
 print("Setup GPIO pin as input on GPIO17")
-io.setup(17, io.IN, pull_up_down=io.PUD_UP)
-io.add_event_detect(17, io.BOTH, callback=sensorCallback, bouncetime=200)
+io.setup(22, io.IN, pull_up_down=io.PUD_UP)
+io.add_event_detect(22, io.BOTH, callback=sensorCallback, bouncetime=200)
 
 if __name__=="__main__":
 	main()
