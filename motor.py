a154ffd665e4bd62fb5b9fb8867d1982c43399c8
chewy441014 <p.hill.ultimate@gmail.com>
2018-05-19 21:08:47 -0700

    fixed

--- motor.py
+++ motor.py
@@ -9,7 +9,6 @@ en_pin = 12
 
 chan_list = [in1_pin, in2_pin, en_pin]
 
-io.setwarnings(False)
 io.setup(chan_list, io.OUT)
 hz = 50
 dt = 1/hz
@@ -31,16 +30,15 @@ def counter_clockwise(duty):
 	p1.start(duty)	
 	
 clockwise(100)
-io.input(in1_pin)
-io.input(in2_pin)
-io.input(en_pin)
+print(io.input(in1_pin))
+print(io.input(in2_pin))
+print(io.input(en_pin))
 enter = raw_input("Press return to stop:")
 counter_clockwise(100)
-io.input(in1_pin)
-io.input(in2_pin)
-io.input(en_pin)
+print(io.input(in1_pin))
+print(io.input(in2_pin))
+print(io.input(en_pin))
 enter = raw_input("Press return to stop:")
 p1.stop
 p2.stop
-io.setup(chan_list, io.IN)
 io.cleanup()
