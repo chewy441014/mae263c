1517bfd968fc5966bfd7162f1680c32d71ab6b36
chewy441014 <p.hill.ultimate@gmail.com>
2018-05-19 20:40:20 -0700

    added opposite direction to test it

--- motor.py
+++ motor.py
@@ -9,6 +9,7 @@ en_pin = 12
 
 chan_list = [in1_pin, in2_pin, en_pin]
 
+io.setwarnings(False)
 io.setup(chan_list, io.OUT)
 hz = 100
 dt = 1/hz
@@ -31,6 +32,9 @@ def counter_clockwise(duty):
 	
 clockwise(50)
 input('Press return to stop:')
+counter_clockwise(50)
+input('Press return to stop:')
 p1.stop
 p2.stop
+io.setup(chan_list, io.IN)
 io.cleanup()
