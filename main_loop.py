44124ff59eefd4b6a682d8f8cea6cae034769bac
chewy441014 <p.hill.ultimate@gmail.com>
2018-06-01 14:34:42 -0700

    moved some stuff, fixed 'prismatic' joint, converted to m everywhere

--- main_loop.py
+++ main_loop.py
@@ -10,8 +10,106 @@ kr = 48
 enc_res = 0.01636246
 special_words = ['BackSpace', 'Tab', 'Enter', 'Cap', 'Shift2', 'Ctrl1', 
 	'WIN1', 'Alt1', 'Alt2', 'WIN2', 'MClick', 'Ctrl2', 'Shift1', '\\']
+L1=0.115 # m
+L2=0.064 # m
+a1=0.018 # m
 
-# set parameters of robot
+# mm units converted at output of keypose
+a0=50 # mm 
+zz=24 # mm
+zs=float (19) #mm
+yy=18 #mm
+k=float (0.048)
+keydic={'Ctrl1':[0,-(a0+20),12],
+		'WIN1':[0,-(a0+20),12+zz],
+		'Alt1':[0,-(a0+20),12+2*zz],
+		' ':[0,-(a0+20),130],
+		'Alt2':[0,-(a0+20),130+12+2*zz],
+		'WIN2':[0,-(a0+20),142+3*zz],
+		'MClick':[0,-(a0+20),142+4*zz],
+		'Ctrl2':[0,-(a0+20),142+5*zz],
+		'Shift1':[float (k*yy),-(a0+20+yy),22],
+		'z':[float (k*yy),-(a0+20+yy),53],
+		'Z':[float (k*yy),-(a0+20+yy),53],
+		'x':[float (k*yy),-(a0+20+yy),53+zs],
+		'X':[float (k*yy),-(a0+20+yy),53+zs],
+		'c':[float (k*yy),-(a0+20+yy),53+2*zs],
+		'C':[float (k*yy),-(a0+20+yy),53+2*zs],
+		'v':[float (k*yy),-(a0+20+yy),53+3*zs],
+		'V':[float (k*yy),-(a0+20+yy),53+3*zs],
+		'b':[float (k*yy),-(a0+20+yy),53+4*zs],
+		'B':[float (k*yy),-(a0+20+yy),53+4*zs],
+		'n':[float (k*yy),-(a0+20+yy),53+5*zs],
+		'N':[float (k*yy),-(a0+20+yy),53+5*zs],
+		'm':[float (k*yy),-(a0+20+yy),53+6*zs],
+		'M':[float (k*yy),-(a0+20+yy),53+6*zs],
+		',':[float (k*yy),-(a0+20+yy),53+7*zs],
+		'.':[float (k*yy),-(a0+20+yy),53+8*zs],
+		'/':[float (k*yy),-(a0+20+yy),53+9*zs],
+		'Shift2':[float (k*yy),-(a0+20+yy),22+10*zs+43],
+		'Cap':[float (2*k*yy),-(a0+20+2*yy),17],
+		'a':[float (2*k*yy),-(a0+20+2*yy),43],
+		'A':[float (2*k*yy),-(a0+20+2*yy),43],
+		's':[float (2*k*yy),-(a0+20+2*yy),43+zs],
+		'S':[float (2*k*yy),-(a0+20+2*yy),43+zs],
+		'd':[float (2*k*yy),-(a0+20+2*yy),43+2*zs],
+		'D':[float (2*k*yy),-(a0+20+2*yy),43+2*zs],
+		'f':[float (2*k*yy),-(a0+20+2*yy),43+3*zs],
+		'F':[float (2*k*yy),-(a0+20+2*yy),43+3*zs],
+		'g':[float (2*k*yy),-(a0+20+2*yy),43+4*zs],
+		'G':[float (2*k*yy),-(a0+20+2*yy),43+4*zs],
+		'h':[float (2*k*yy),-(a0+20+2*yy),43+5*zs],
+		'H':[float (2*k*yy),-(a0+20+2*yy),43+5*zs],
+		'j':[float (2*k*yy),-(a0+20+2*yy),43+6*zs],
+		'J':[float (2*k*yy),-(a0+20+2*yy),43+6*zs],
+		'k':[float (2*k*yy),-(a0+20+2*yy),43+7*zs],
+		'K':[float (2*k*yy),-(a0+20+2*yy),43+7*zs],
+		'l':[float (2*k*yy),-(a0+20+2*yy),43+8*zs],
+		'L':[float (2*k*yy),-(a0+20+2*yy),43+8*zs],
+		';':[float (2*k*yy),-(a0+20+2*yy),43+9*zs],
+		'\':[float (2*k*yy),-(a0+20+2*yy),43+10*zs],
+		'Enter':[float (2*k*yy),-(a0+20+2*yy),33+20+11*zs],
+		'Tab':[float (3*k*yy),-(a0+20+3*yy),15],
+		'q':[float (3*k*yy),-(a0+20+3*yy),39],
+		'Q':[float (3*k*yy),-(a0+20+3*yy),39],
+		'w':[float (3*k*yy),-(a0+20+3*yy),zs+39],
+		'W':[float (3*k*yy),-(a0+20+3*yy),zs+39],
+		'e':[float (3*k*yy),-(a0+20+3*yy),2*zs+39],
+		'E':[float (3*k*yy),-(a0+20+3*yy),2*zs+39],
+		'r':[float (3*k*yy),-(a0+20+3*yy),3*zs+39],
+		'R':[float (3*k*yy),-(a0+20+3*yy),3*zs+39],
+		't':[float (3*k*yy),-(a0+20+3*yy),4*zs+39],
+		'T':[float (3*k*yy),-(a0+20+3*yy),4*zs+39],
+		'y':[float (3*k*yy),-(a0+20+3*yy),5*zs+39],
+		'Y':[float (3*k*yy),-(a0+20+3*yy),5*zs+39],
+		'u':[float (3*k*yy),-(a0+20+3*yy),6*zs+39],
+		'U':[float (3*k*yy),-(a0+20+3*yy),6*zs+39],
+		'i':[float (3*k*yy),-(a0+20+3*yy),7*zs+39],
+		'I':[float (3*k*yy),-(a0+20+3*yy),7*zs+39],
+		'o':[float (3*k*yy),-(a0+20+3*yy),8*zs+39],
+		'O':[float (3*k*yy),-(a0+20+3*yy),8*zs+39],
+		'p':[float (3*k*yy),-(a0+20+3*yy),9*zs+39],
+		'P':[float (3*k*yy),-(a0+20+3*yy),9*zs+39],
+		'[':[float (3*k*yy),-(a0+20+3*yy),10*zs+39],
+		']':[float (3*k*yy),-(a0+20+3*yy),11*zs+39],
+		'\\':[float (3*k*yy),-(a0+20+3*yy),12*zs+30+14],
+		'`':[float (4*k*yy),-(a0+20+4*yy),float (zs/2)],
+		'1':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+zs)],
+		'2':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+2*zs)],
+		'3':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+3*zs)],
+		'4':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+4*zs)],
+		'5':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+5*zs)],
+		'6':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+6*zs)],
+		'7':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+7*zs)],
+		'8':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+8*zs)],
+		'9':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+9*zs)],
+		'0':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+10*zs)],
+		'-':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+11*zs)],
+		'=':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+12*zs)],
+		'BackSpace':[float (4*k*yy),-(a0+20+4*yy),13*zs+19]
+		}
+	
+# set parameters of robot (SI UNITS)
 a1,a2=0.115,0.064
 len_link1=0.07
 len_link2=0.04#distances of centers of mass from joint axes
@@ -193,11 +291,8 @@ def encoder3Callback(channel):
 	A3_t1 = A3_t2
 io.add_event_detect(en5_pin, io.BOTH, callback=encoder3Callback)
 
-def invskinem(pose=[0,-100,0]):
-     L1=115
-     L2=64
-     a1=18
-     d1=pose[2]
+def invskinem(pose=[0,-.1,0]):
+     d1=pose[2]/(math.pi*r_pulley*2)
      c3=float ((pose[0]-a1)**2+pose[1]**2-L1**2-L2**2)/(2*L1*L2)
      try:
          s3=-math.sqrt(1-c3**2)
@@ -210,102 +305,9 @@ def invskinem(pose=[0,-100,0]):
      return [d1,th2,th3]
 	 
 def keypose(read):
-    a0=50
-    zz=24
-    zs=float (19)
-    yy=18
-    k=float (0.048)
-    keydic={'Ctrl1':[0,-(a0+20),12],
-            'WIN1':[0,-(a0+20),12+zz],
-            'Alt1':[0,-(a0+20),12+2*zz],
-            ' ':[0,-(a0+20),130],
-            'Alt2':[0,-(a0+20),130+12+2*zz],
-            'WIN2':[0,-(a0+20),142+3*zz],
-            'MClick':[0,-(a0+20),142+4*zz],
-            'Ctrl2':[0,-(a0+20),142+5*zz],
-            'Shift1':[float (k*yy),-(a0+20+yy),22],
-            'z':[float (k*yy),-(a0+20+yy),53],
-            'Z':[float (k*yy),-(a0+20+yy),53],
-            'x':[float (k*yy),-(a0+20+yy),53+zs],
-            'X':[float (k*yy),-(a0+20+yy),53+zs],
-            'c':[float (k*yy),-(a0+20+yy),53+2*zs],
-            'C':[float (k*yy),-(a0+20+yy),53+2*zs],
-            'v':[float (k*yy),-(a0+20+yy),53+3*zs],
-            'V':[float (k*yy),-(a0+20+yy),53+3*zs],
-            'b':[float (k*yy),-(a0+20+yy),53+4*zs],
-            'B':[float (k*yy),-(a0+20+yy),53+4*zs],
-            'n':[float (k*yy),-(a0+20+yy),53+5*zs],
-            'N':[float (k*yy),-(a0+20+yy),53+5*zs],
-            'm':[float (k*yy),-(a0+20+yy),53+6*zs],
-            'M':[float (k*yy),-(a0+20+yy),53+6*zs],
-            ',':[float (k*yy),-(a0+20+yy),53+7*zs],
-            '.':[float (k*yy),-(a0+20+yy),53+8*zs],
-            '/':[float (k*yy),-(a0+20+yy),53+9*zs],
-            'Shift2':[float (k*yy),-(a0+20+yy),22+10*zs+43],
-            'Cap':[float (2*k*yy),-(a0+20+2*yy),17],
-            'a':[float (2*k*yy),-(a0+20+2*yy),43],
-            'A':[float (2*k*yy),-(a0+20+2*yy),43],
-            's':[float (2*k*yy),-(a0+20+2*yy),43+zs],
-            'S':[float (2*k*yy),-(a0+20+2*yy),43+zs],
-            'd':[float (2*k*yy),-(a0+20+2*yy),43+2*zs],
-            'D':[float (2*k*yy),-(a0+20+2*yy),43+2*zs],
-            'f':[float (2*k*yy),-(a0+20+2*yy),43+3*zs],
-            'F':[float (2*k*yy),-(a0+20+2*yy),43+3*zs],
-            'g':[float (2*k*yy),-(a0+20+2*yy),43+4*zs],
-            'G':[float (2*k*yy),-(a0+20+2*yy),43+4*zs],
-            'h':[float (2*k*yy),-(a0+20+2*yy),43+5*zs],
-            'H':[float (2*k*yy),-(a0+20+2*yy),43+5*zs],
-            'j':[float (2*k*yy),-(a0+20+2*yy),43+6*zs],
-            'J':[float (2*k*yy),-(a0+20+2*yy),43+6*zs],
-            'k':[float (2*k*yy),-(a0+20+2*yy),43+7*zs],
-            'K':[float (2*k*yy),-(a0+20+2*yy),43+7*zs],
-            'l':[float (2*k*yy),-(a0+20+2*yy),43+8*zs],
-            'L':[float (2*k*yy),-(a0+20+2*yy),43+8*zs],
-            ';':[float (2*k*yy),-(a0+20+2*yy),43+9*zs],
-            '\':[float (2*k*yy),-(a0+20+2*yy),43+10*zs],
-            'Enter':[float (2*k*yy),-(a0+20+2*yy),33+20+11*zs],
-            'Tab':[float (3*k*yy),-(a0+20+3*yy),15],
-            'q':[float (3*k*yy),-(a0+20+3*yy),39],
-            'Q':[float (3*k*yy),-(a0+20+3*yy),39],
-            'w':[float (3*k*yy),-(a0+20+3*yy),zs+39],
-            'W':[float (3*k*yy),-(a0+20+3*yy),zs+39],
-            'e':[float (3*k*yy),-(a0+20+3*yy),2*zs+39],
-            'E':[float (3*k*yy),-(a0+20+3*yy),2*zs+39],
-            'r':[float (3*k*yy),-(a0+20+3*yy),3*zs+39],
-            'R':[float (3*k*yy),-(a0+20+3*yy),3*zs+39],
-            't':[float (3*k*yy),-(a0+20+3*yy),4*zs+39],
-            'T':[float (3*k*yy),-(a0+20+3*yy),4*zs+39],
-            'y':[float (3*k*yy),-(a0+20+3*yy),5*zs+39],
-            'Y':[float (3*k*yy),-(a0+20+3*yy),5*zs+39],
-            'u':[float (3*k*yy),-(a0+20+3*yy),6*zs+39],
-            'U':[float (3*k*yy),-(a0+20+3*yy),6*zs+39],
-            'i':[float (3*k*yy),-(a0+20+3*yy),7*zs+39],
-            'I':[float (3*k*yy),-(a0+20+3*yy),7*zs+39],
-            'o':[float (3*k*yy),-(a0+20+3*yy),8*zs+39],
-            'O':[float (3*k*yy),-(a0+20+3*yy),8*zs+39],
-            'p':[float (3*k*yy),-(a0+20+3*yy),9*zs+39],
-            'P':[float (3*k*yy),-(a0+20+3*yy),9*zs+39],
-            '[':[float (3*k*yy),-(a0+20+3*yy),10*zs+39],
-            ']':[float (3*k*yy),-(a0+20+3*yy),11*zs+39],
-            '\\':[float (3*k*yy),-(a0+20+3*yy),12*zs+30+14],
-            '`':[float (4*k*yy),-(a0+20+4*yy),float (zs/2)],
-            '1':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+zs)],
-            '2':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+2*zs)],
-            '3':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+3*zs)],
-            '4':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+4*zs)],
-            '5':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+5*zs)],
-            '6':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+6*zs)],
-            '7':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+7*zs)],
-            '8':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+8*zs)],
-            '9':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+9*zs)],
-            '0':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+10*zs)],
-            '-':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+11*zs)],
-            '=':[float (4*k*yy),-(a0+20+4*yy),float (zs/2+12*zs)],
-            'BackSpace':[float (4*k*yy),-(a0+20+4*yy),13*zs+19]
-            }
     if read in keydic:
         output=keydic.get(read)
-        return output
+        return [output[0]/1000, output[1]/1000, output[2]/1000]
     else:
         print('Whoops! No keys found!')
 		return
@@ -313,7 +315,7 @@ def keypose(read):
 def control1(pos_d):
 	# initialize the encoders
 	##################################################
-	#This is for motor3 control
+	#This is for motor1 control
 	##################################################
 	tolerance=0.01
 	pos_error1=100
@@ -325,7 +327,7 @@ def control1(pos_d):
 		elif pos_error1<0:
 			clockwise(duty_cycle_1,p1,p2,m1_en_pin)
 	##################################################
-	#This is for motor1 and motor2 control
+	#This is for motor2 and motor3 control
 	##################################################
 	tolerance=0.1
 	position_error=[100,100]
@@ -403,15 +405,25 @@ def taskcontrol(command_list):
 		nearest_home = [current_pos[0], 0, -math.pi/2]
 		if (current_pos[1] != nearest_home[1]) or (current_pos[2] != nearest_home[2])
 			control1(nearest_home)
+			print("I'm Home!")
+			time.sleep(1)
 		cart_pos_d = keypose(command_list[i])
 		# getpose(key_desired)
 		joint_pos_d = invskinem(cart_pos_d)
 		# inverse kinematics to find joint space position
+		print("Move to "+command_list[i])
+		print("Cartesian Coordinates")
+		print(cart_pos_d)
+		print("Joint Space Coordinates")
+		print(joint_pos_d)
 		control1(joint_pos_d)
+		print("Motion Complete!")
+		time.sleep(1)
 		# control(pose_desired)
 	# end for loop
 	abs_home = [0, 0, -math.pi/2]
 	# return to global home position
+	print("Going to Absolute Home Position")
 	control1(abs_home)
 
 def setparameter():
