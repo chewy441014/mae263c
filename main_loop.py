1d8f409c0e1f70080c5cf3ed18ab79fa8378f721
chewy441014 <p.hill.ultimate@gmail.com>
2018-06-02 16:34:23 -0700

    called it

--- main_loop.py
+++ main_loop.py
@@ -520,9 +520,11 @@ def test2():
 	p2.stop()
 
 enter = raw_input("Ready??")
-print("GO!!!")
-resetEncoders()
-command_list = ['g']
-taskcontrol(command_list)
-io.cleanup()
-print("Task Complete")
+#print("GO!!!")
+#resetEncoders()
+#command_list = ['g']
+#taskcontrol(command_list)
+#io.cleanup()
+#print("Task Complete")
+
+test1()
