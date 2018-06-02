0d53d390ce733463cca008305972b8b8c1b87390
chewy441014 <p.hill.ultimate@gmail.com>
2018-06-02 15:57:23 -0700

    progress

--- main_loop.py
+++ main_loop.py
@@ -307,8 +307,6 @@ def encoder3Callback(channel):
 io.add_event_detect(en5_pin, io.BOTH, callback=encoder3Callback)
 
 def invskinem(pose=[0,-.1,0]):
-	for i in range(3):
-		pose[i] = pose[i]*1000
 	d1=pose[2]/(r_pulley)
 	c3=float(((pose[0]-a1)**2+pose[1]**2-L1**2-L2**2)/(2*L1*L2))
 	try:
