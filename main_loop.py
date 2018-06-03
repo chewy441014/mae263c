8f2a8ab7c642a98648553e9c7608911ac7735168
chewy441014 <p.hill.ultimate@gmail.com>
2018-06-02 23:35:07 -0700

    less verbose

--- main_loop.py
+++ main_loop.py
@@ -344,7 +344,6 @@ def control1(pos_d):
 		print("Controlling motor 1")
 		while abs(pos_error1) >=tolerance:
 			pos_error1=pos_d[0]-countstorad(encoder1_count)
-			print(countstorad(encoder1_count))
 			duty_cycle_1=100
 			if pos_error1>0:
 				clockwise(duty_cycle_1, p1, p2, m1_en_pin)
@@ -458,7 +457,6 @@ def control1(pos_d):
 		while abs(position_error) > tolerance:
 			# get current position
 			pos_current=countstorad(encoder2_count)
-			print(countstorad(encoder2_count))
 			# get position error
 			position_error=pos_d[1]-pos_current
 			if position_error<0:
