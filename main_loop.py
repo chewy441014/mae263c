a699ac7ac7e353e6921a93ec68f7a755435d2489
chewy441014 <p.hill.ultimate@gmail.com>
2018-06-02 20:50:56 -0700

    fixed a bug

--- main_loop.py
+++ main_loop.py
@@ -440,9 +440,9 @@ def control1(pos_d):
 			pos_current=countstorad(encoder3_count)
 			# get position error
 			position_error=pos_d[2]-pos_current
-			if position_error[1]<0:
+			if position_error<0:
 				clockwise(duty_cycle_3, p5, p6, m3_en_pin)
-			elif position_error[1]>0:
+			elif position_error>0:
 				clockwise(100-duty_cycle_3, p5, p6, m3_en_pin)
 		print("Current Position [rad]")
 		print([countstorad(encoder1_count),countstorad(encoder2_count),countstorad(encoder3_count)])
